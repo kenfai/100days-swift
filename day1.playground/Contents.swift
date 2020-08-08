93eff4642ac291ce1863fa16941c6841e4b9cba9
KenFai <ngkenfai@gmail.com>
2020-08-08 22:21:07 +0800

    day 1 complete

---
+++ day1.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+var str = "Hello, playground"
+
+str = "Goodbye"
+
+var age = 38
+
+age = 8_000_000
+
+var multiline = """
+Today is \
+indeed a \
+good day.
+"""
+
+var pi = 3.14159
+
+var isNew = true
+
+var sentence = "The value of pi is \(pi)"
+
+let michael = "jackson"
+
+let album: String = "Reputation"
+let year: Int = 2020
+let sales: Double = 70.89
+let isPlatinum: Bool = true
