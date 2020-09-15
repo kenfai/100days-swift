e2f4c0b40cae7f006a980e3b4f916294099df87b
KenFai <ngkenfai@gmail.com>
2020-09-15 23:36:14 +0800

    Day 10, part 1: CupcakeCorner

---
+++ CupcakeCorner/CupcakeCorner/AlbumView.swift
@@ -0,0 +1,86 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Ginger on 15/09/2020.
+//  Copyright © 2020 Ginger. All rights reserved.
+//
+
+import SwiftUI
+
+class User: ObservableObject, Codable {
+    @Published var name = "Paul Hudson"
+    
+    enum CodingKeys: CodingKey {
+        case name
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+    }
+}
+
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
+struct AlbumView: View {
+    @State private var results = [Result]()
+    
+    
+    var body: some View {
+        //Text("Hello, World!")
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+            }
+        }.onAppear(perform: loadData)
+    }
+    
+    func loadData () {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                    // we have good data – go back to the main thread
+                    DispatchQueue.main.async {
+                        // update our UI
+                        self.results = decodedResponse.results
+                    }
+                    
+                    // everything is good, so we can exit
+                    return
+                }
+            }
+            
+            // if we're still here it means there was a problem
+            print("Fetch failed: \(error?.localizedDescription ?? "Unknown Error")")
+        }.resume()
+    }
+}
+
+struct AlbumView_Previews: PreviewProvider {
+    static var previews: some View {
+        AlbumView()
+    }
+}
