306209997f36702ac60b3ffd9f06d60d3e428e1c
KenFai <ngkenfai@gmail.com>
2020-09-29 23:58:46 +0800

    Day 71 - Project 14, BucketList part 4

---
+++ BucketList/BucketList/MKPointAnnotation-ObservableObject.swift
@@ -0,0 +1,30 @@
+//
+//  MKPointAnnotation-ObservableObject.swift
+//  BucketList
+//
+//  Created by Ginger on 29/09/2020.
+//
+
+import MapKit
+
+extension MKPointAnnotation: ObservableObject {
+    public var wrappedTitle: String {
+        get {
+            self.title ?? "Unknown value"
+        }
+        
+        set {
+            title = newValue
+        }
+    }
+    
+    public var wrappedSubtitle: String {
+        get {
+            self.subtitle ?? "Unknown value"
+        }
+        
+        set {
+            subtitle = newValue
+        }
+    }
+}
