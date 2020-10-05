8a4f072875817ea87cb6a0035cfddd0aa46df286
KenFai <ngkenfai@gmail.com>
2020-10-05 22:39:10 +0800

    Day 79~81 - Project 16, part one~three

---
+++ HotProspects/HotProspects/Kind.swift
@@ -0,0 +1,18 @@
+//
+//  Kind.swift
+//  HotProspects
+//
+//  Created by Ginger on 05/10/2020.
+//
+
+import Foundation
+
+class MyKind {
+    func hello() {
+        
+    }
+    
+    func hello(_ k: Int) {
+        
+    }
+}
